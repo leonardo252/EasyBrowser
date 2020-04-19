e0fda9fded6191379afeb0b7ddc6531eb7589f98
Leonardo Gomes Fernandes <leonardo-lol252@hotmail.com>
2020-04-19 20:35:13 -0300

    Initial Commit

---
+++ EasyBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Leonardo Gomes on 19/04/20.
+//  Copyright © 2020 Leonardo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
